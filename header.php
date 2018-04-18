25313b1002c676eb889239e01cc47a875df6421b
petr <pilarpe4@fit.cvut.cz>
2018-04-18 12:41:05 +0200

    initial commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Extended boolean model</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="main-wrapper">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include_once 'header.php' ?>
+
+<section class="main-container">
+    <div class="main-wrapper">
+        <h2>Extended boolean model</h2>
+        <form>
+            <ul class="form-style-1">
+                <li>
+                    <label>Query <span class="required">*</span></label>
+                    <input type="text" name="query" class="field-long" />
+                </li>
+                <li>
+                    <label>Number of results <span class="required">*</span></label>
+                    <input type="number" name="n_of_res" class="field-long" />
+                </li>
+                <li>
+                    <input type="submit" value="Find" />
+                </li>
+            </ul>
+        </form>
+        <h2>Results</h2>
+        <div id="results" >
+            <dl>
+                <dt>Black hot drink Black hot drink</dt>
+                <dd>Black hot drink</dd>
+
+                <dt>Milk</dt>
+                <dd>White cold drink</dd>
+
+                <dt>Milk</dt>
+                <dd>White cold drink</dd>
+
+                <dt>Milk</dt>
+                <dd>White cold drink</dd>
+
+                <dt>Milk</dt>
+                <dd>White cold drink</dd>
+            </dl>
+        </div>
+    </div>
+</section>
+
+<?php include_once 'footer.php' ?>
