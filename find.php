8eb6db080b2cdff572d9de7a5cd859627d5db3a8
petr <pilarpe4@fit.cvut.cz>
2018-05-04 23:48:04 +0200

    naa

--- find.php
+++ find.php
@@ -20,7 +20,7 @@
                 <?php
                 if (isset($_GET["query"]) && isset($_GET["number"])) {
                     $file = 'src\\infix_to_postfix_and_evaluate.exe';
-                    $input = $_GET["number"] . " " . $_GET["query"];
+                    $input = $_GET["number"] . ' "' . $_GET["query"] . '"';
 
                     if (file_exists($file)) {
                         echo "<p>file " . $file . " exists</p>";
@@ -28,15 +28,21 @@
                         echo "<p>file " . $file . " does not exists</p>";
                     }
 
-                    $return = -1;
+                    $return = 5;
                     $toexec = $file . " " . $input;
-                    exec($toexec, $out, $return);
 
-                    var_dump($out);
+                    exec($toexec, $lines, $return);
+                    //                    $lines = system($toexec, $return);
+                    var_dump($toexec);
+                    echo "<br>";
+                    var_dump($lines);
+                    echo "<br>";
                     var_dump($return);
+                    echo "<br>";
 
-                    foreach ($out as $item) {
+                    foreach ($lines as $item) {
                         echo '<a href="./data/doc' . $item . '.txt">' . 'doc' . $item . '.txt' . "</a><br>";
+                        echo "<br>";
                     }
                 } else {
                     echo "<p>its working but sadly not now (=_=)</p>";
--- src/infix_to_postfix_and_evaluate.cpp
+++ src/infix_to_postfix_and_evaluate.cpp
@@ -78,79 +78,79 @@ static string InfixToPostfix(string in_infix) {
 int documentCount = 0;
 
 double opNOT(double weight) {
-	return 1 - weight;
+    return 1 - weight;
 }
 
 /*static Lemma opNOT(Lemma lemma) {
-	Lemma res(lemma.m_LemmaName);
+        Lemma res(lemma.m_LemmaName);
 	
-	//lemma.m_Docs ordered by docId by default, so we can use little trick
-	for(int i = 1; i <= documentCount; i++) {
-		if (i == (lemma.m_Docs.front()).m_Id) {
-			lemma.m_Docs.pop_front(); //push nothing and pop from reference lemma 
-		}
-		else {
-			res.m_Docs.push_back(LemmaElem(i, computeWeightForNot())); //else add to our res
-		}
-	}
+        //lemma.m_Docs ordered by docId by default, so we can use little trick
+        for(int i = 1; i <= documentCount; i++) {
+                if (i == (lemma.m_Docs.front()).m_Id) {
+                        lemma.m_Docs.pop_front(); //push nothing and pop from reference lemma 
+                }
+                else {
+                        res.m_Docs.push_back(LemmaElem(i, computeWeightForNot())); //else add to our res
+                }
+        }
 	
-	return res;
+        return res;
 }*/
 
 double opAND(double weightA, double weightB) {
-	double termCount = 2.0, 
-		   res = 1 - sqrt(((1 - weightA)*(1 - weightA) + (1 - weightB)*(1 - weightB)) / termCount);
-	return res;
+    double termCount = 2.0,
+            res = 1 - sqrt(((1 - weightA)*(1 - weightA) + (1 - weightB)*(1 - weightB)) / termCount);
+    return res;
 }
 
 /*static Lemma opAND(Lemma & lemma1, Lemma & lemma2) {
-	Lemma res("res");
-	auto it1 = lemma1.m_Docs.begin();
-	auto it2 = lemma2.m_Docs.begin();
-	//lists are ordered by docId
-	while(it1 != lemma1.m_Docs.end() && it2 != lemma2.m_Docs.end()) {
-		if (it1->m_Id == it2->m_Id) {
-			res.PushBackDoc(it1->m_Id, computeWeightForAnd(it1->m_Weight, it2->m_Weight));
-			it1++;
-			it2++;
-		}
-		else if (it1->m_Id < it2->m_Id) {
-			it1++;
-		}
-		else {
-			it2++;
-		}
-	}
-	return res;
+        Lemma res("res");
+        auto it1 = lemma1.m_Docs.begin();
+        auto it2 = lemma2.m_Docs.begin();
+        //lists are ordered by docId
+        while(it1 != lemma1.m_Docs.end() && it2 != lemma2.m_Docs.end()) {
+                if (it1->m_Id == it2->m_Id) {
+                        res.PushBackDoc(it1->m_Id, computeWeightForAnd(it1->m_Weight, it2->m_Weight));
+                        it1++;
+                        it2++;
+                }
+                else if (it1->m_Id < it2->m_Id) {
+                        it1++;
+                }
+                else {
+                        it2++;
+                }
+        }
+        return res;
 }*/
 
 double opOR(double weightA, double weightB) {
-	double termCount = 2.0, 
-		   res = sqrt((weightA*weightA + weightB*weightB) / termCount);
-	return res;
+    double termCount = 2.0,
+            res = sqrt((weightA * weightA + weightB * weightB) / termCount);
+    return res;
 }
 
 /*static Lemma opOR(Lemma & lemma1, Lemma & lemma2) {
     Lemma res("res");
-	auto it1 = lemma1.m_Docs.begin();
-	auto it2 = lemma2.m_Docs.begin();
-	//lists are ordered by docId
-	while(it1 != lemma1.m_Docs.end() || it2 != lemma2.m_Docs.end()) {
-		if (it1->m_Id == it2->m_Id) {
-			res.PushBackDoc(it1->m_Id, computeWeightForAnd(it1->m_Weight, it2->m_Weight));
-			it1++;
-			it2++;
-		}
-		else if (it1->m_Id < it2->m_Id) {
-			res.PushBackDoc(it1->m_Id, computeWeightForAnd(it1->m_Weight, it2->m_Weight));
-			it1++;
-		}
-		else {
-			res.PushBackDoc(it2->m_Id, computeWeightForAnd(it1->m_Weight, it2->m_Weight));
-			it2++;
-		}
-	}
-	return res;
+        auto it1 = lemma1.m_Docs.begin();
+        auto it2 = lemma2.m_Docs.begin();
+        //lists are ordered by docId
+        while(it1 != lemma1.m_Docs.end() || it2 != lemma2.m_Docs.end()) {
+                if (it1->m_Id == it2->m_Id) {
+                        res.PushBackDoc(it1->m_Id, computeWeightForAnd(it1->m_Weight, it2->m_Weight));
+                        it1++;
+                        it2++;
+                }
+                else if (it1->m_Id < it2->m_Id) {
+                        res.PushBackDoc(it1->m_Id, computeWeightForAnd(it1->m_Weight, it2->m_Weight));
+                        it1++;
+                }
+                else {
+                        res.PushBackDoc(it2->m_Id, computeWeightForAnd(it1->m_Weight, it2->m_Weight));
+                        it2++;
+                }
+        }
+        return res;
 }*/
 
 /*static Lemma evaluate(string in_postfix, LemmaContainer & lemmaContainer, WordPairContainer & wordPairContainer) {
@@ -164,8 +164,8 @@ double opOR(double weightA, double weightB) {
         cerr << "ERROR: prvni term v postfixu je operator!!" << endl;
         exit(1);
     }
-	string lemmaString = wordPairContainer.FindLemma(term);
-	Lemma lemma = lemmaContainer.FindLemma(term);
+        string lemmaString = wordPairContainer.FindLemma(term);
+        Lemma lemma = lemmaContainer.FindLemma(term);
     zasobnik.push(lemma);
     while (ss >> term) {
         if (isOperator(term)) {
@@ -189,8 +189,8 @@ double opOR(double weightA, double weightB) {
             }
         } else {
             //jedná se o operand --> pouze pridame do zasobniku
-			string lemmaString = wordPairContainer.FindLemma(term); //returns "" if not successful
-			Lemma lemma = lemmaContainer.FindLemma(lemmaString); //returns empty Lemma (with empty list of ids) if not succesful
+                        string lemmaString = wordPairContainer.FindLemma(term); //returns "" if not successful
+                        Lemma lemma = lemmaContainer.FindLemma(lemmaString); //returns empty Lemma (with empty list of ids) if not succesful
             zasobnik.push(lemma);
         }
 
@@ -203,14 +203,14 @@ static double evaluate(string in_postfix, int docId, LemmaContainer & lemmaConta
 
     stringstream ss(in_postfix);
     string term;
-	
+
     ss >> term;
     if (isOperator(term)) {
         cerr << "ERROR: prvni term v postfixu je operator!!" << endl;
         exit(1);
     }
-	string lemmaString = wordPairContainer.FindLemma(term);
-	Lemma lemma = lemmaContainer.FindLemma(lemmaString);
+    string lemmaString = wordPairContainer.FindLemma(term);
+    Lemma lemma = lemmaContainer.FindLemma(lemmaString);
     zasobnik.push(lemma.GetWeightByDocId(docId));
     while (ss >> term) {
         if (isOperator(term)) {
@@ -234,8 +234,8 @@ static double evaluate(string in_postfix, int docId, LemmaContainer & lemmaConta
             }
         } else {
             //jedná se o operand --> pouze pridame do zasobniku
-			string lemmaString = wordPairContainer.FindLemma(term); //returns "" if not successful
-			Lemma lemma = lemmaContainer.FindLemma(lemmaString); //returns empty Lemma (with empty list of ids) if not succesful
+            string lemmaString = wordPairContainer.FindLemma(term); //returns "" if not successful
+            Lemma lemma = lemmaContainer.FindLemma(lemmaString); //returns empty Lemma (with empty list of ids) if not succesful
             zasobnik.push(lemma.GetWeightByDocId(docId));
         }
 
@@ -244,54 +244,54 @@ static double evaluate(string in_postfix, int docId, LemmaContainer & lemmaConta
 }
 
 bool orderPairIntDoubleByDouble(const pair<int, double> & A, const pair<int, double> & B) {
-	return A.second > B.second;
+    return A.second > B.second;
 }
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
-		cerr << "Argument count != 3. Usage: exe [resultCount] query." << endl;
-		return -1;
-	}
-	
-	documentCount = docCount();
-	
+    if (argc != 3) {
+        cerr << "Argument count != 3. Usage: exe [resultCount] query." << endl;
+        return -1;
+    }
+
+    documentCount = docCount();
+
     //string infix = "kocka AND pes AND ( NOT opice OR krava )";
-	int resCount = atoi(argv[1]);
-	string infix(argv[2]);
-	
-	//init pairs word/term (cats/cat, sang/sing...)
-	WordPairContainer wordPairContainer;
-	
-	//init inverted index structure
-	LemmaContainer lemmaContainer; //container of inverted indexes
-	lemmaContainer.InitContainer(); //get structure from file
-	//lemmaContainer.PrintContainer(); //for debugging
-	
+    int resCount = atoi(argv[1]);
+    string infix(argv[2]);
+
+    //init pairs word/term (cats/cat, sang/sing...)
+    WordPairContainer wordPairContainer;
+
+    //init inverted index structure
+    LemmaContainer lemmaContainer; //container of inverted indexes
+    lemmaContainer.InitContainer(); //get structure from file
+    //lemmaContainer.PrintContainer(); //for debugging
+
     //cout << "INFIX:   " << infix << endl;
     string postfix = InfixToPostfix(infix); //kocka pes AND opice NOT krava OR AND
     //cout << "POSTFIX: " << postfix << endl;
-	
-	list<pair<int, double> > resList; //pair of docId and relevancy
-	double relevancy = 0.0;
-	for(int i = 1; i <= documentCount; i++) {
-		relevancy = evaluate(postfix, i, lemmaContainer, wordPairContainer); //returns relevancy for each document
-		resList.push_back(make_pair(i, relevancy));
-		//cout << i << " " << relevancy << endl;
-	}
-	//cout << "**************" << endl;
-	
-	//order documents in result by relevancy
-	resList.sort(orderPairIntDoubleByDouble);
-	
-	//return on std out list of document Ids
-	//only docIds plus newline: 2\r\n4\r\n7...
-	int index = 0;
-	for(auto x : resList) {
-		if (index >= resCount) break; //maximum of resCount
-		cout << x.first << endl; //for app run
-		//cout << x.first << " " << x.second << endl; //for debugging
-		index++;
-	}
+
+    list<pair<int, double> > resList; //pair of docId and relevancy
+    double relevancy = 0.0;
+    for (int i = 1; i <= documentCount; i++) {
+        relevancy = evaluate(postfix, i, lemmaContainer, wordPairContainer); //returns relevancy for each document
+        resList.push_back(make_pair(i, relevancy));
+        //cout << i << " " << relevancy << endl;
+    }
+    //cout << "**************" << endl;
+
+    //order documents in result by relevancy
+    resList.sort(orderPairIntDoubleByDouble);
+
+    //return on std out list of document Ids
+    //only docIds plus newline: 2\r\n4\r\n7...
+    int index = 0;
+    for (auto x : resList) {
+        if (index >= resCount) break; //maximum of resCount
+        cout << x.first << endl; //for app run
+        //cout << x.first << " " << x.second << endl; //for debugging
+        index++;
+    }
 
     return 0;
 }
