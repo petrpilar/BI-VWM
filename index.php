3cedb2517830b10f8dfa115ef65b903e9c118afb
petr <pilarpe4@fit.cvut.cz>
2018-05-05 08:15:11 +0200

    uyz to funguje

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <section class="main-container">
             <div class="main-wrapper">
                 <h2>Extended boolean model</h2>
-                <form method="get" action="find.php">
+                <form method="get" action="src/find.php">
                     <ul class="form-style-1">
                         <li>
                             <label>Query <span class="required">*</span></label>
--- src/find.php
+++ src/find.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Extended boolean model</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="../style.css">
     </head>
     <body>
         <header>
@@ -19,30 +19,31 @@
                 <h1>Results</h1>
                 <?php
                 if (isset($_GET["query"]) && isset($_GET["number"])) {
-                    $file = 'src\\infix_to_postfix_and_evaluate.exe';
+                    //$file = 'test.exe';
+                    $file = 'infix_to_postfix_and_evaluate.exe';
                     $input = $_GET["number"] . ' "' . $_GET["query"] . '"';
 
                     if (file_exists($file)) {
                         echo "<p>file " . $file . " exists</p>";
                     } else {
-                        echo "<p>file " . $file . " does not exists</p>";
+                        echo "<p>file " . $file . " does NOT exists</p>";
                     }
 
                     $return = 5;
                     $toexec = $file . " " . $input;
-
                     exec($toexec, $lines, $return);
-                    //                    $lines = system($toexec, $return);
-                    var_dump($toexec);
-                    echo "<br>";
-                    var_dump($lines);
-                    echo "<br>";
-                    var_dump($return);
-                    echo "<br>";
+
+                    /*
+                      var_dump($toexec);
+                      echo "<br>";
+                      var_dump($lines);
+                      echo "<br>";
+                      var_dump($return);
+                      echo "<br>";
+                     */
 
                     foreach ($lines as $item) {
-                        echo '<a href="./data/doc' . $item . '.txt">' . 'doc' . $item . '.txt' . "</a><br>";
-                        echo "<br>";
+                        echo '<a href="../docs/doc' . $item . '.txt">' . 'doc' . $item . '.txt' . "</a><br>";
                     }
                 } else {
                     echo "<p>its working but sadly not now (=_=)</p>";
